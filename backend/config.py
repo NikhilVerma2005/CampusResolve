e870c8ee07acbd4ffa1bd609ad61f17c9f726577
NikhilVerma2005 <nikhilverma31032005@gmail.com>
2026-02-16 17:08:12 +0530

    f

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///instance/database.db"
+        "sqlite:///database.db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
