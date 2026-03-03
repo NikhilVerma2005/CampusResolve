d1cf086d29eb3000b7339f00aa59f4a737d608dc
NikhilVerma2005 <nikhilverma31032005@gmail.com>
2026-03-03 16:06:56 +0530

    update delayed time for loader page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,42 @@
-import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import StudentDashboard from "./pages/StudentDashboard";
 import OfficeDashboard from "./pages/OfficeDashboard";
 
-const API_URL = "https://campusresolve-05m5.onrender.com"; 
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [serverReady, setServerReady] = useState(false);
+  const [showLoader, setShowLoader] = useState(false);
 
   useEffect(() => {
     const wakeServer = async () => {
-      let ready = false;
+      // Start timer — only show loader after 2 seconds
+      const timer = setTimeout(() => {
+        setShowLoader(true);
+      }, 2000);
 
-      while (!ready) {
-        try {
-          const res = await fetch(`${API_URL}/health`);
-          if (res.ok) {
-            ready = true;
-            setServerReady(true);
-          }
-        } catch (err) {
-          console.log("Backend sleeping...");
+      try {
+        const res = await fetch(`${API_URL}/health`);
+        if (res.ok) {
+          clearTimeout(timer);
+          setServerReady(true);
         }
+      } catch (err) {
+        setShowLoader(true);
 
-        if (!ready) {
+        // Retry if sleeping
+        let ready = false;
+        while (!ready) {
+          try {
+            const retry = await fetch(`${API_URL}/health`);
+            if (retry.ok) {
+              ready = true;
+              setServerReady(true);
+            }
+          } catch {}
           await new Promise((r) => setTimeout(r, 10000));
         }
       }
@@ -35,23 +45,22 @@ function App() {
     wakeServer();
   }, []);
 
-
-  if (!serverReady) {
-  return (
-    <div className="loading-container">
-      <h1 className="namaste">🙏🏻नमस्ते </h1>
-
-      <div className="progress-bar">
-        <div className="progress"></div>
+  if (!serverReady && showLoader) {
+    return (
+      <div className="loading-container">
+        <h1 className="namaste">नमस्ते</h1>
+        <div className="progress-bar">
+          <div className="progress"></div>
+        </div>
+        <h2>Initializing Server...</h2>
+        <p className="small-text">
+          First request may take up to 60 seconds (free hosting).
+        </p>
       </div>
+    );
+  }
 
-      <h2>Initializing Server...</h2>
-      <p className="small-text">
-        First request may take up to 60 seconds (free hosting).
-      </p>
-    </div>
-  );
-}
+  if (!serverReady) return null; // Don't flash loader
 
   return (
     <Routes>
